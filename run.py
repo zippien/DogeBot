4811d50e69621d06139b57154f574518edf8a4e7
zippien <arulblood@gmail.com>
2019-05-10 01:01:47 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python2
+#iya dong bangsat
+#Otak guwe otak kepiting keras sikit!
+import os
+import sys
+os.system('clear')
+print ('\n\n')
+print ('\033[1;96m ╦ ╦╔═╗╦  ╔═╗╔═╗╔╦╗╔═╗ ')
+print (' ║║║║╣ ║  ║  ║ ║║║║║╣   ')
+print (' ╚╩╝╚═╝╩═╝╚═╝╚═╝╩ ╩╚═╝ ')
+print (' zippien recode ')
+print ('\033[0;90m    [\033[1;36m+\033[0;90m] Enter your phone_number')
+print ('\033[0;90m    [\033[1;36m+\033[0;90m] Example: +62895365636822')
+doge = input('\033[0;90m    [\033[1;36m+\033[0;90m] Your Number: \033[1;36m')
+os.system('cd .core && python main.py ' + doge)
